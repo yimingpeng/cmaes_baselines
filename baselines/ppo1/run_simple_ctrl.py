2894aa8bec4b6effb89330c5b023a4bba7e67cda
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-06-05 16:05:58 +1200

    Test PPO1 on simple control problem and pybullet problem to see how many steps it needs to complete the task

--- baselines/ppo1/run_simple_ctrl.py
+++ baselines/ppo1/run_simple_ctrl.py
@@ -17,13 +17,13 @@ def train(env_id, num_timesteps, seed):
     U.make_session(num_cpu=1).__enter__()
     def policy_fn(name, ob_space, ac_space):
         return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
-            hid_size=16, num_hid_layers=2)
+            hid_size=16, num_hid_layers=1)
     env = make_simple_control_env(env_id, seed)
     pposgd_simple.learn(env, policy_fn,
             max_timesteps=num_timesteps,
             timesteps_per_actorbatch=2048,
             clip_param=0.2, entcoeff=0.0,
-            optim_epochs=10, optim_stepsize=3e-4, optim_batchsize=64,
+            optim_epochs=10, optim_stepsize=3e-4, optim_batchsize=128,
             gamma=0.99, lam=0.95, schedule='linear'
         )
     env.close()
