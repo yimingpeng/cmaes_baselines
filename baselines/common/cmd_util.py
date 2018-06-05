58c3ae82081c24396699284caa9b2729451c8f20
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-06-05 16:05:32 +1200

    Change the maximum steps allowance

--- baselines/common/cmd_util.py
+++ baselines/common/cmd_util.py
@@ -125,7 +125,7 @@ def simple_ctrl_arg_parser():
     parser.add_argument('--env', help='environment ID', type=str,
                         default="CartPole-v0")
     parser.add_argument('--seed', help='RNG seed', type=int, default=1)
-    parser.add_argument('--num-timesteps', type=int, default=int(1e6))
+    parser.add_argument('--num-timesteps', type=int, default=int(1e10))
     return parser
 
 
@@ -140,5 +140,5 @@ def pybullet_arg_parser():
     parser.add_argument('--env', help='environment ID', type=str,
                         default="InvertedPendulumBulletEnv-v0")
     parser.add_argument('--seed', help='RNG seed', type=int, default=1)
-    parser.add_argument('--num-timesteps', type=int, default=int(2e6))
+    parser.add_argument('--num-timesteps', type=int, default=int(2e8))
     return parser
