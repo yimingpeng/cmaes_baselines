986aa6eb729cc580fb1e7452cb840749a90de53c
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-09-18 16:36:50 +1200

    Modify the parameters for uniform

--- baselines/ppo_cmaes_surrogate1_uniform_local_search_s1/pposgd_simple.py
+++ baselines/ppo_cmaes_surrogate1_uniform_local_search_s1/pposgd_simple.py
@@ -368,9 +368,9 @@ def learn(env, policy_fn, *,
         else:
             raise NotImplementedError
 
-        epsilon = max(1.0 - float(timesteps_so_far) / (0.5 * max_timesteps), 0) * cur_lrmult
+        epsilon = max(0.5 - float(timesteps_so_far) / (10 * max_timesteps), 0) * cur_lrmult
         # epsilon = 0.2
-        sigma_adapted = max(1.0 - float(timesteps_so_far) / (0.5 * max_timesteps), 1e-8)
+        sigma_adapted = max(sigma - float(timesteps_so_far) / (10 * max_timesteps), 1e-8)
         logger.log("********** Iteration %i ************" % iters_so_far)
         eval_seg = eval_seq.__next__()
         rewbuffer.extend(eval_seg["ep_rets"])
