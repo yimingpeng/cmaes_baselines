18aae285fc82614f1294540fc0d32a65a757de52
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-09-18 14:47:11 +1200

    Modify the parameters for uniform

--- baselines/ppo_cmaes_surrogate1_uniform/pposgd_simple.py
+++ baselines/ppo_cmaes_surrogate1_uniform/pposgd_simple.py
@@ -360,7 +360,7 @@ def learn(env, policy_fn, *,
 
         epsilon = max(0.5 - float(timesteps_so_far)  / max_timesteps, 0) * cur_lrmult
         # epsilon = 0.2
-        sigma_adapted = max(sigma - float(timesteps_so_far) / max_timesteps, 1e-8)
+        sigma_adapted = max(sigma - float(timesteps_so_far) * 10 / max_timesteps, 1e-8)
         logger.log("********** Iteration %i ************" % iters_so_far)
         eval_seg = eval_seq.__next__()
         rewbuffer.extend(eval_seg["ep_rets"])
