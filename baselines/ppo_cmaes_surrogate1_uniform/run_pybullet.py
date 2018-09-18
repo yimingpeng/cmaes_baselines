eacac270ae126bf40c3aa7b5d273a7a046350ea3
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-09-19 11:58:10 +1200

    1. Adjust parameter for uniform

--- baselines/ppo_cmaes_surrogate1_uniform/run_pybullet.py
+++ baselines/ppo_cmaes_surrogate1_uniform/run_pybullet.py
@@ -20,8 +20,8 @@ from baselines import logger
 
 def train(env_id, num_timesteps, seed):
     max_fitness = -100000
-    popsize = 10
-    gensize = 250 # For each iterations
+    popsize = 5
+    gensize = 50 # For each iterations
     max_v_train_iter = 1
     bounds = [-5.0, 5.0]
     sigma = 3e-5
