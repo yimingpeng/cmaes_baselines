377963ce3fba7aaf6ede0925141e6044e5db616f
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-09-24 10:55:52 +1200

    1. Adjust the parameters 2. Clip the sigma in cma

--- baselines/common/cmd_util.py
+++ baselines/common/cmd_util.py
@@ -145,7 +145,7 @@ def gym_ctrl_arg_parser():
     parser.add_argument('--env', help='environment ID', type=str,
                         default="BipedalWalker-v2")
     parser.add_argument('--seed', help='RNG seed', type=int, default=1)
-    parser.add_argument('--num-timesteps', type=int, default=int(2e7))
+    parser.add_argument('--num-timesteps', type=int, default=int(1e7))
     return parser
 
 
@@ -162,5 +162,5 @@ def pybullet_arg_parser():
     # parser.add_argument('--env', help='environment ID', type=str,
     #                     default="HumanoidBulletEnv-v0")
     parser.add_argument('--seed', help='RNG seed', type=int, default=1)
-    parser.add_argument('--num-timesteps', type=int, default=int(2e7))
+    parser.add_argument('--num-timesteps', type=int, default=int(1e7))
     return parser
