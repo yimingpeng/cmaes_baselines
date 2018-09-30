5880caabb116693b6507a121c1d55d23d6943a83
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-01 09:11:00 +1300

    1. Adjust simga 2. Add sigma_fac

--- baselines/ppo_cmaes_surrogate1_uniform/pposgd_simple.py
+++ baselines/ppo_cmaes_surrogate1_uniform/pposgd_simple.py
@@ -510,8 +510,8 @@ def learn(env, policy_fn, *,
                     break
                 # logger.log("Iteration:" + str(iters_so_far) + " - sub-train Generation for Policy:" + str(es.countiter))
                 # logger.log("Sigma=" + str(es.sigma))
-                # solutions = es.ask(sigma_fac = max(cur_lrmult, 1e-8))
-                solutions = es.ask()
+                solutions = es.ask(sigma_fac = max(cur_lrmult, 1e-8))
+                # solutions = es.ask()
                 # solutions = [np.clip(solution, -5.0, 5.0).tolist() for solution in solutions]
                 costs = []
                 lens = []
--- baselines/ppo_cmaes_surrogate1_uniform/run_pybullet.py
+++ baselines/ppo_cmaes_surrogate1_uniform/run_pybullet.py
@@ -25,7 +25,7 @@ def train(env_id, num_timesteps, seed):
     max_v_train_iter = 10
     bounds = [-5.0, 5.0]
     # sigma = 0.001
-    sigma = 5e-4
+    sigma = 3e-4
     eval_iters = 1
     from baselines.ppo_cmaes_surrogate1_uniform import mlp_policy, pposgd_simple
     U.make_session(num_cpu=1).__enter__()
