a67298027f37db25c204ae10e733e75d857d8162
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-09 07:05:10 +1300

    1. Nes implemenation 2. uniform readjust the parameter

--- baselines/simple_nes/run_gym_ctrl.py
+++ baselines/simple_nes/run_gym_ctrl.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import inspect
+# Add the current folder to PYTHONPATH by Yiming
 import os
 import sys
 
@@ -7,26 +7,21 @@ sys.path.append(
     os.path.abspath(
         os.path.join(
             os.path.abspath(os.path.join(os.getcwd(), os.pardir)), os.pardir)))
-currentdir = os.path.dirname(
-    os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(os.path.dirname(currentdir))
-os.sys.path.insert(0, parentdir)
 
-# very important, don't remove, otherwise pybullet cannot run (reasons are unknown)
-import pybullet_envs
-from baselines.common.cmd_util import pybullet_arg_parser, make_pybullet_env
+from baselines.common.cmd_util import gym_ctrl_arg_parser, \
+    make_gym_control_env
 from baselines.common import tf_util as U
 from baselines import logger
 
 
 def train(env_id, num_timesteps, seed):
-    max_fitness = -100000
-    popsize = 5
-    gensize = 10000
+    max_fitness = -10000
+    popsize = 128
+    gensize = 2000
     bounds = [-5.0, 5.0]
     sigma = 0.1
     eval_iters = 3
-    from baselines.nes import mlp_policy, nes_simple
+    from baselines.cmaes import mlp_policy, cmaes_simple
     U.make_session(num_cpu=1).__enter__()
 
     def policy_fn(name, ob_space, ac_space):
@@ -34,8 +29,8 @@ def train(env_id, num_timesteps, seed):
                                     ac_space=ac_space,
                                     hid_size=64, num_hid_layers=2)
 
-    base_env = make_pybullet_env(env_id, seed)
-    nes_simple.learn(base_env,
+    base_env = make_gym_control_env(env_id, seed)
+    cmaes_simple.learn(base_env,
                        policy_fn,
                        max_fitness = max_fitness,  # has to be negative, as cmaes consider minization
                        popsize = popsize,
@@ -45,16 +40,13 @@ def train(env_id, num_timesteps, seed):
                        eval_iters = eval_iters,
                        max_timesteps=num_timesteps,
                        timesteps_per_actorbatch=2048,
-                       seed=seed,
-                       optim_stepsize = 3e-5,
-                       schedule='linear'
-                       )
+                       seed=seed)
     base_env.close()
 
 
 def main():
-    args = pybullet_arg_parser().parse_args()
-    logger.configure(format_strs=['stdout', 'log', 'csv'], log_suffix = "NES-"+args.env)
+    args = gym_ctrl_arg_parser().parse_args()
+    logger.configure(format_strs=['stdout', 'log', 'csv'], log_suffix = "CMAES-"+args.env)
     train(args.env, num_timesteps=args.num_timesteps, seed=args.seed)
 
 
