7131ea73a25a53a7ce5452e9f9275858f476b3af
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-11 09:29:21 +1300

    1. Train results only

--- baselines/ppo_cmaes_surrogate1_uniform/pposgd_simple.py
+++ baselines/ppo_cmaes_surrogate1_uniform/pposgd_simple.py
@@ -383,11 +383,11 @@ def learn(env, test_env, policy_fn, *,
         epsilon = max(0.5 - float(timesteps_so_far) / (max_timesteps), 0) * cur_lrmult
         # epsilon = 0.2
         sigma_adapted = max(max(sigma - float(timesteps_so_far) / (5000 * max_timesteps), 0) * cur_lrmult, 1e-8)
-        # cmean_adapted = max(1.0 - float(timesteps_so_far) / (max_timesteps), 1e-8)
-        cmean_adapted = 1.0
+        cmean_adapted = max(1.0 - float(timesteps_so_far) / (max_timesteps), 1e-8)
+        # cmean_adapted = 1.0
         # if timesteps_so_far % max_timesteps == 10:
-        # max_v_train_iter = int(max(max_v_train_iter * (1 - timesteps_so_far/(0.5*max_timesteps)), 1))
-        max_v_train_iter = 10
+        max_v_train_iter = int(max(max_v_train_iter * (1 - timesteps_so_far/(0.5*max_timesteps)), 1))
+        # max_v_train_iter = 10
         logger.log("********** Iteration %i ************" % iters_so_far)
         if iters_so_far == 0:
             eval_seg = eval_seq.__next__()
