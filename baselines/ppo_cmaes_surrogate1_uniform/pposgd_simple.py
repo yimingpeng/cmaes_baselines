704d743d196011c6ae78073d236a9ac080fc2765
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-11 11:57:07 +1300

    1. Record only test

--- baselines/ppo_cmaes_surrogate1_uniform/pposgd_simple.py
+++ baselines/ppo_cmaes_surrogate1_uniform/pposgd_simple.py
@@ -406,8 +406,8 @@ def learn(env, policy_fn, *,
         add_vtarg_and_adv(seg, gamma, lam)
         if hasattr(pi, "ob_rms"): pi.ob_rms.update(seg["ob"])  # update running mean/std for normalization
 
-        rewbuffer.extend(seg["ep_rets"])
-        lenbuffer.extend(seg["ep_lens"])
+        # rewbuffer.extend(seg["ep_rets"])
+        # lenbuffer.extend(seg["ep_lens"])
 
         assign_old_eq_new()  # set old parameter values to new parameter values
         if segs is None:
