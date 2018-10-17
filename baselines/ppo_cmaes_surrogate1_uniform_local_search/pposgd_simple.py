688649ea41152e322771ce17af14f6c7e51fe3c7
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-17 16:40:26 +1300

    1. Remove clip for sigma in CMAES 2. Change the parameters for local search

--- baselines/ppo_cmaes_surrogate1_uniform_local_search/pposgd_simple.py
+++ baselines/ppo_cmaes_surrogate1_uniform_local_search/pposgd_simple.py
@@ -281,10 +281,10 @@ def learn(env, policy_fn, *,
             "final")] + logstd_var_list)
 
     vf_lossandgrad = U.function([ob, ac, ret, lrmult],
-                                vf_losses + [U.flatgrad(vf_loss, vf_var_list, 20.0)])
+                                vf_losses + [U.flatgrad(vf_loss, vf_var_list, 40.0)])
 
     lossandgrad = U.function([ob, ac, atarg, ret, lrmult, layer_clip],
-                             losses + [U.flatgrad(total_loss, var_list, 20.0)])
+                             losses + [U.flatgrad(total_loss, var_list, 40.0)])
 
     vf_adam = MpiAdam(vf_var_list, epsilon = adam_epsilon)
     adam = MpiAdam(var_list, epsilon = adam_epsilon)
@@ -380,7 +380,7 @@ def learn(env, policy_fn, *,
         if schedule == 'constant':
             cur_lrmult = 1.0
         elif schedule == 'linear':
-            cur_lrmult = max(1.0 - float(timesteps_so_far) / (0.5 * max_timesteps), 1e-8)
+            cur_lrmult = max(1.0 - float(timesteps_so_far) / (max_timesteps), 1e-8)
 
         else:
             raise NotImplementedError
@@ -391,7 +391,7 @@ def learn(env, policy_fn, *,
         # cmean_adapted = max(1.0 - float(timesteps_so_far) / (max_timesteps), 1e-8)
         # cmean_adapted = max(0.8 - float(time˚steps_so_far) / (2*max_timesteps), 1e-8)
         # if timesteps_so_far % max_timesteps == 10:
-        max_v_train_iter = int(max(max_v_train_iter * (1 - timesteps_so_far/(0.5*max_timesteps)), 1))
+        # max_v_train_iter = int(max(max_v_train_iter * (1 - timesteps_so_far/(0.5*max_timesteps)), 1))
         logger.log("********** Iteration %i ************" % iters_so_far)
         if iters_so_far == 0:
             eval_seg = eval_seq.__next__()
