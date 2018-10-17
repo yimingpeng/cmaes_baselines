f83f19d6dada3169eb3e78624452669708fb05ce
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-17 14:43:23 +1300

    1. Restore back to original to test

--- baselines/ppo_cmaes_surrogate1_uniform/pposgd_simple.py
+++ baselines/ppo_cmaes_surrogate1_uniform/pposgd_simple.py
@@ -28,7 +28,7 @@ def traj_segment_generator_eval(pi, env, horizon, stochastic):
 
     while True:
         ac, vpred, a_prop = pi.act(stochastic, ob)
-        ac = np.clip(ac, env.action_space.low, env.action_space.high)
+        # ac = np.clip(ac, env.action_space.low, env.action_space.high)
         # Slight weirdness here because we need value function at time T
         # before returning segment [0, T-1] so we get the correct
         # terminal value
@@ -88,7 +88,7 @@ def traj_segment_generator(pi, env, horizon, stochastic, eval_seq):
             # result_record()
         prevac = ac
         ac, vpred, act_prop = pi.act(stochastic, ob)
-        ac = np.clip(ac, env.action_space.low, env.action_space.high)
+        # ac = np.clip(ac, env.action_space.low, env.action_space.high)
         # Slight weirdness here because we need value function at time T
         # before returning segment [0, T-1] so we get the correct
         # terminal value
