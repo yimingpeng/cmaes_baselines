d814e55714943529b2fa5b1460ac135af16180b9
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-18 11:30:49 +1300

    1. Remove clipping seperation

--- baselines/ppo1/pposgd_simple.py
+++ baselines/ppo1/pposgd_simple.py
@@ -9,8 +9,48 @@ from mpi4py import MPI
 from collections import deque
 
 
+def traj_segment_generator_eval(pi, env, horizon, stochastic):
+    t = 0
+    ob = env.reset()
 
-def traj_segment_generator(pi, env, horizon, stochastic):
+    cur_ep_ret = 0  # return in current episode
+    cur_ep_len = 0  # len of current episode
+    ep_rets = []  # returns of completed episodes in this segment
+    ep_lens = []  # lengths of ...
+    ep_num = 0
+    while True:
+        ac, vpred = pi.act(stochastic, ob)
+        # ac = np.clip(ac, env.action_space.low, env.action_space.high)
+        # ac = np.clip(ac, env.action_space.low, env.action_space.high)
+        # Slight weirdness here because we need value function at time T
+        # before returning segment [0, T-1] so we get the correct
+        # terminal value
+        if t > 0 and t % horizon == 0 and ep_num >= 5:
+            yield {"ep_rets": ep_rets, "ep_lens": ep_lens}
+            # Be careful!!! if you change the downstream algorithm to aggregate
+            # several of these batches, then be sure to do a deepcopy
+            ep_rets = []
+            ep_lens = []
+            ep_num = 0
+            cur_ep_ret = 0
+            cur_ep_len = 0
+            ob = env.reset()
+
+        ob, rew, new, _ = env.step(ac)
+
+        cur_ep_ret += rew
+        cur_ep_len += 1
+        if new:
+            ep_num += 1
+            ep_rets.append(cur_ep_ret)
+            ep_lens.append(cur_ep_len)
+            cur_ep_ret = 0
+            cur_ep_len = 0
+            ob = env.reset()
+        t += 1
+
+
+def traj_segment_generator(pi, env, horizon, stochastic, eval_seq):
     global timesteps_so_far
     t = 0
     ac = env.action_space.sample()  # not used, just so we have the datatype
@@ -41,9 +81,6 @@ def traj_segment_generator(pi, env, horizon, stochastic):
         # before returning segment [0, T-1] so we get the correct
         # terminal value
         if t > 0 and t % horizon == 0:
-            if record:
-                result_record()
-                record = False
             yield {"ob": obs, "rew": rews, "vpred": vpreds, "new": news,
                    "ac": acs, "prevac": prevacs, "nextvpred": vpred * (1 - new),
                    "ep_rets": ep_rets, "ep_lens": ep_lens}
@@ -53,6 +90,7 @@ def traj_segment_generator(pi, env, horizon, stochastic):
             ep_lens = []
             cur_ep_ret = 0
             cur_ep_len = 0
+            ob = env.reset()
         i = t % horizon
         obs[i] = ob
         vpreds[i] = vpred
@@ -69,6 +107,9 @@ def traj_segment_generator(pi, env, horizon, stochastic):
         timesteps_so_far += 1
         if new:
             if record:
+                eval_seg = eval_seq.__next__()
+                rewbuffer.extend(eval_seg["ep_rets"])
+                lenbuffer.extend(eval_seg["ep_lens"])
                 result_record()
                 record = False
             ep_rets.append(cur_ep_ret)
@@ -144,7 +185,7 @@ def learn(env, policy_fn, *,
 
     ob = U.get_placeholder_cached(name = "ob")
     ac = pi.pdtype.sample_placeholder([None])
-
+    import numpy as np
     kloldnew = oldpi.pd.kl(pi.pd)
     ent = pi.pd.entropy()
     meankl = tf.reduce_mean(kloldnew)
@@ -161,8 +202,8 @@ def learn(env, policy_fn, *,
     loss_names = ["pol_surr", "pol_entpen", "vf_loss", "kl", "ent"]
 
     var_list = pi.get_trainable_variables()
-    lossandgrad = U.function([ob, ac, atarg, ret, lrmult], losses + [U.flatgrad(total_loss, var_list)])
-    adam = MpiAdam(var_list, epsilon=adam_epsilon)
+    lossandgrad = U.function([ob, ac, atarg, ret, lrmult], losses + [U.flatgrad(total_loss, var_list, 40.0)])
+    adam = MpiAdam(var_list, epsilon = adam_epsilon)
 
     assign_old_eq_new = U.function([], [], updates = [tf.assign(oldv, newv)
                                                       for (oldv, newv) in
@@ -174,7 +215,10 @@ def learn(env, policy_fn, *,
 
     # Prepare for rollouts
     # ----------------------------------------
-    seg_gen = traj_segment_generator(pi, env, timesteps_per_actorbatch, stochastic = True)
+    eval_seq = traj_segment_generator_eval(pi, env,
+                                           timesteps_per_actorbatch,
+                                           stochastic = False)
+    seg_gen = traj_segment_generator(pi, env, timesteps_per_actorbatch, stochastic = True, eval_seq=eval_seq)
 
     global timesteps_so_far, episodes_so_far, iters_so_far, \
         tstart, lenbuffer, rewbuffer, best_fitness
@@ -202,26 +246,26 @@ def learn(env, policy_fn, *,
         if schedule == 'constant':
             cur_lrmult = 1.0
         elif schedule == 'linear':
-            cur_lrmult =  max(1.0 - float(timesteps_so_far) / max_timesteps, 0)
+            cur_lrmult = max(1.0 - float(timesteps_so_far) / (max_timesteps), 0)
         else:
             raise NotImplementedError
 
         logger.log("********** Iteration %i ************" % iters_so_far)
-        # if iters_so_far == 0:
-        #     eval_seg = eval_seq.__next__()
-        #     rewbuffer.extend(eval_seg["ep_rets"])
-        #     lenbuffer.extend(eval_seg["ep_lens"])
-        #     result_record()
+        if iters_so_far == 0:
+            eval_seg = eval_seq.__next__()
+            rewbuffer.extend(eval_seg["ep_rets"])
+            lenbuffer.extend(eval_seg["ep_lens"])
+            result_record()
 
 
         seg = seg_gen.__next__()
-        lrlocal = (seg["ep_lens"], seg["ep_rets"])  # local values
-        listoflrpairs = MPI.COMM_WORLD.allgather(lrlocal)  # list of tuples
-        lens, rews = map(flatten_lists, zip(*listoflrpairs))
-        lenbuffer.extend(lens)
-        rewbuffer.extend(rews)
-        if iters_so_far == 0:
-            result_record()
+        # lrlocal = (seg["ep_lens"], seg["ep_rets"])  # local values
+        # listoflrpairs = MPI.COMM_WORLD.allgather(lrlocal)  # list of tuples
+        # lens, rews = map(flatten_lists, zip(*listoflrpairs))
+        # lenbuffer.extend(lens)
+        # rewbuffer.extend(rews)
+        # if iters_so_far == 0:
+        #     result_record()
 
         add_vtarg_and_adv(seg, gamma, lam)
 
