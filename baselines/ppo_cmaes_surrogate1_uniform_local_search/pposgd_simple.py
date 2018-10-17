1b27b1d6bb144d18cb77543266be3fa37e80f7b4
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-17 17:31:25 +1300

    1. Fix some bugs, should show the same results now

--- baselines/ppo_cmaes_surrogate1_uniform_local_search/pposgd_simple.py
+++ baselines/ppo_cmaes_surrogate1_uniform_local_search/pposgd_simple.py
@@ -384,9 +384,11 @@ def learn(env, policy_fn, *,
         else:
             raise NotImplementedError
 
-        epsilon = max(0.5 - float(timesteps_so_far) / (max_timesteps), 0) * cur_lrmult
+        # epsilon = max(0.5 - float(timesteps_so_far) / (max_timesteps), 0) * cur_lrmult
+        epsilon = max(0.5 * cur_lrmult, 0)
         # epsilon = 0.2
-        sigma_adapted = max(max(sigma - float(timesteps_so_far) / (5000 * max_timesteps), 0) * cur_lrmult, 1e-8)
+        sigma_adapted = max(sigma * cur_lrmult, 1e-8)
+        # sigma_adapted = max(max(sigma - float(timesteps_so_far) / (5000 * max_timesteps), 0) * cur_lrmult, 1e-8)
         # cmean_adapted = max(1.0 - float(timesteps_so_far) / (max_timesteps), 1e-8)
         # cmean_adapted = max(0.8 - float(time˚steps_so_far) / (2*max_timesteps), 1e-8)
         # if timesteps_so_far % max_timesteps == 10:
