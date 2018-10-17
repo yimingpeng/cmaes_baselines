01fb65d684991cc9783c8687abc9c11942128bda
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-17 14:34:03 +1300

    1. Restore back to original to test

--- baselines/ppo_cmaes_surrogate1_uniform/pposgd_simple.py
+++ baselines/ppo_cmaes_surrogate1_uniform/pposgd_simple.py
@@ -283,7 +283,7 @@ def learn(env, policy_fn, *,
             "final")] + logstd_var_list)
 
     vf_lossandgrad = U.function([ob, ac, ret, lrmult],
-                                vf_losses + [U.flatgrad(vf_loss, vf_var_list, 40.0)])
+                                vf_losses + [U.flatgrad(vf_loss, vf_var_list)])
 
 
     vf_adam = MpiAdam(vf_var_list, epsilon = adam_epsilon)
