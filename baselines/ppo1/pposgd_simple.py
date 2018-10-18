6c814878b629fb45328f83b7d5068f184467cd3e
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-18 14:47:26 +1300

    1. Modify the maximum learning steps to 2e6 2. Modify the decay to 0.5

--- baselines/ppo1/pposgd_simple.py
+++ baselines/ppo1/pposgd_simple.py
@@ -247,7 +247,7 @@ def learn(env, policy_fn, *,
         if schedule == 'constant':
             cur_lrmult = 1.0
         elif schedule == 'linear':
-            cur_lrmult = max(1.0 - float(timesteps_so_far) / (0.25 * max_timesteps), 0)
+            cur_lrmult = max(1.0 - float(timesteps_so_far) / (0.5 * max_timesteps), 0)
         else:
             raise NotImplementedError
 
