8f7800058c22206f8dafebc23429d8427b887cc6
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-18 14:40:16 +1300

    1. Modify the maximum learning steps to 2e5

--- baselines/common/cmd_util.py
+++ baselines/common/cmd_util.py
@@ -147,7 +147,7 @@ def gym_ctrl_arg_parser():
     parser.add_argument('--env', help='environment ID', type=str,
                         default="BipedalWalker-v2")
     parser.add_argument('--seed', help='RNG seed', type=int, default=1)
-    parser.add_argument('--num-timesteps', type=int, default=int(1e7))
+    parser.add_argument('--num-timesteps', type=int, default=int(2e6))
     return parser
 
 
@@ -164,5 +164,5 @@ def pybullet_arg_parser():
     # parser.add_argument('--env', help='environment ID', type=str,
     #                     default="HumanoidBulletEnv-v0")
     parser.add_argument('--seed', help='RNG seed', type=int, default=1)
-    parser.add_argument('--num-timesteps', type=int, default=int(1e7))
+    parser.add_argument('--num-timesteps', type=int, default=int(2e6))
     return parser
