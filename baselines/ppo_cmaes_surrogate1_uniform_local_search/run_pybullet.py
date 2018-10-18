18679d849da4e3c1cf34bc596ccaa7826a4147dc
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-10-18 16:19:17 +1300

    1. Decrease the learning rate to 3e-4

--- baselines/ppo_cmaes_surrogate1_uniform_local_search/run_pybullet.py
+++ baselines/ppo_cmaes_surrogate1_uniform_local_search/run_pybullet.py
@@ -23,7 +23,7 @@ def train(env_id, num_timesteps, seed):
     gensize = 150 # For each iterations
     max_v_train_iter = 10
     bounds = [-5.0, 5.0]
-    sigma = 0.01
+    sigma = 3e-4
     eval_iters = 1
     from baselines.ppo_cmaes_surrogate1_uniform_local_search import mlp_policy, pposgd_simple
     U.make_session(num_cpu=1).__enter__()
