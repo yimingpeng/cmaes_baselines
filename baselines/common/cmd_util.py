7c8ebe9b49464d879594d8081cf5ef8ac151b896
pengyiming <yiming.peng@ecs.vuw.ac.nz>
2018-11-16 14:15:40 +1300

    Modify Uber-GA lr

--- baselines/common/cmd_util.py
+++ baselines/common/cmd_util.py
@@ -6,7 +6,7 @@ import pybullet
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(os.path.dirname(currentdir))
-os.sys.path.insert(0,parentdir)
+os.sys.path.insert(0, parentdir)
 import os
 import gym
 from gym.wrappers import FlattenDictWrapper
@@ -16,36 +16,46 @@ from baselines.common import set_global_seeds
 from baselines.common.atari_wrappers import make_atari, wrap_deepmind
 from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
 
-def make_atari_env(env_id, num_env, seed, wrapper_kwargs=None, start_index=0):
+
+def make_atari_env(env_id, num_env, seed, wrapper_kwargs = None, start_index = 0):
     """
     Create a wrapped, monitored SubprocVecEnv for Atari.
     """
     if wrapper_kwargs is None: wrapper_kwargs = {}
-    def make_env(rank): # pylint: disable=C0111
+
+    def make_env(rank):  # pylint: disable=C0111
         def _thunk():
             env = make_atari(env_id)
             env.seed(seed + rank)
             env = Monitor(env, logger.get_dir() and os.path.join(logger.get_dir(), str(rank)))
             return wrap_deepmind(env, **wrapper_kwargs)
+
         return _thunk
+
     set_global_seeds(seed)
     return SubprocVecEnv([make_env(i + start_index) for i in range(num_env)])
 
-def make_gym_control_multi_env(env_id, num_env, seed, wrapper_kwargs=None, start_index=0):
+
+def make_gym_control_multi_env(env_id, num_env, seed, wrapper_kwargs = None, start_index = 0):
     """
     Added by Yiming (29/5/2018)
     Create a wrapped, monitored gym.Env for Simple Control Problems.
     """
     if wrapper_kwargs is None: wrapper_kwargs = {}
-    def make_env(rank): # pylint: disable=C0111
+
+    def make_env(rank):  # pylint: disable=C0111
         def _thunk():
             env = gym.make(env_id)
             env.seed(seed + rank)
-            env = Monitor(env, logger.get_dir() and os.path.join(logger.get_dir(), str(rank)), allow_early_resets=True)
+            env = Monitor(env, logger.get_dir() and os.path.join(logger.get_dir(), str(rank)),
+                          allow_early_resets = True)
             return env
+
         return _thunk
+
     set_global_seeds(seed)
     return SubprocVecEnv([make_env(i + start_index) for i in range(num_env)])
+
 
 def make_mujoco_env(env_id, seed):
     """
@@ -57,7 +67,8 @@ def make_mujoco_env(env_id, seed):
     env.seed(seed)
     return env
 
-def make_robotics_env(env_id, seed, rank=0):
+
+def make_robotics_env(env_id, seed, rank = 0):
     """
     Create a wrapped, monitored gym.Env for MuJoCo.
     """
@@ -66,9 +77,10 @@ def make_robotics_env(env_id, seed, rank=0):
     env = FlattenDictWrapper(env, ['observation', 'desired_goal'])
     env = Monitor(
         env, logger.get_dir() and os.path.join(logger.get_dir(), str(rank)),
-        info_keywords=('is_success',))
+        info_keywords = ('is_success',))
     env.seed(seed)
     return env
+
 
 def make_gym_control_env(env_id, seed):
     """
@@ -80,6 +92,7 @@ def make_gym_control_env(env_id, seed):
     # env = Monitor(env, logger.get_dir(), allow_early_resets=True)
     env.seed(seed)
     return env
+
 
 def make_pybullet_env(env_id, seed):
     """
@@ -96,43 +109,48 @@ def make_pybullet_env(env_id, seed):
     env.seed(seed)
     return env
 
+
 def arg_parser():
     """
     Create an empty argparse.ArgumentParser.
     """
     import argparse
-    return argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    return argparse.ArgumentParser(formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+
 
 def atari_arg_parser():
     """
     Create an argparse.ArgumentParser for run_atari.py.
     """
     parser = arg_parser()
-    parser.add_argument('--env', help='environment ID', default='BreakoutNoFrameskip-v4')
-    parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-    parser.add_argument('--num-timesteps', type=int, default=int(10e6))
+    parser.add_argument('--env', help = 'environment ID', default = 'BreakoutNoFrameskip-v4')
+    parser.add_argument('--seed', help = 'RNG seed', type = int, default = 0)
+    parser.add_argument('--num-timesteps', type = int, default = int(10e6))
     return parser
+
 
 def mujoco_arg_parser():
     """
     Create an argparse.ArgumentParser for run_mujoco.py.
     """
     parser = arg_parser()
-    parser.add_argument('--env', help='environment ID', type=str, default='Reacher-v2')
-    parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-    parser.add_argument('--num-timesteps', type=int, default=int(1e6))
-    parser.add_argument('--play', default=False, action='store_true')
+    parser.add_argument('--env', help = 'environment ID', type = str, default = 'Reacher-v2')
+    parser.add_argument('--seed', help = 'RNG seed', type = int, default = 0)
+    parser.add_argument('--num-timesteps', type = int, default = int(1e6))
+    parser.add_argument('--play', default = False, action = 'store_true')
     return parser
+
 
 def robotics_arg_parser():
     """
     Create an argparse.ArgumentParser for run_mujoco.py.
     """
     parser = arg_parser()
-    parser.add_argument('--env', help='environment ID', type=str, default='FetchReach-v0')
-    parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-    parser.add_argument('--num-timesteps', type=int, default=int(1e6))
+    parser.add_argument('--env', help = 'environment ID', type = str, default = 'FetchReach-v0')
+    parser.add_argument('--seed', help = 'RNG seed', type = int, default = 0)
+    parser.add_argument('--num-timesteps', type = int, default = int(1e6))
     return parser
+
 
 def gym_ctrl_arg_parser():
     """
@@ -143,11 +161,11 @@ def gym_ctrl_arg_parser():
     # parser.add_argument('--env', help='environment ID', type=str,
     #                     default="LunarLander-v2")
     # parser.add_argument('--env', help='environment ID', type=str,
-        #                     default="LunarLanderContinuous-v2")
-    parser.add_argument('--env', help='environment ID', type=str,
-                        default="BipedalWalker-v2")
-    parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-    parser.add_argument('--num-timesteps', type=int, default=int(5e6))
+    #                     default="LunarLanderContinuous-v2")
+    parser.add_argument('--env', help = 'environment ID', type = str,
+                        default = "BipedalWalker-v2")
+    parser.add_argument('--seed', help = 'RNG seed', type = int, default = 0)
+    parser.add_argument('--num-timesteps', type = int, default = int(5e6))
     return parser
 
 
@@ -161,12 +179,12 @@ def pybullet_arg_parser():
     #                     default="AntBulletEnv-v0")
     # parser.add_argument('--env', help='environment ID', type=str,
     #                     default="InvertedPendulumSwingupBulletEnv-v0")
-    parser.add_argument('--env', help='environment ID', type=str,
-                        default="InvertedPendulumBulletEnv-v0")
+    parser.add_argument('--env', help = 'environment ID', type = str,
+                        default = "InvertedPendulumBulletEnv-v0")
     # parser.add_argument('--env', help='environment ID', type=str,
     #                     default="Walker2DBulletEnv-v0")
     # parser.add_argument('--env', help='environment ID', type=str,
     #                     default="HumanoidBulletEnv-v0")
-    parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-    parser.add_argument('--num-timesteps', type=int, default=int(5e6))
+    parser.add_argument('--seed', help = 'RNG seed', type = int, default = 0)
+    parser.add_argument('--num-timesteps', type = int, default = int(5e6))
     return parser
--- baselines/uber_ga/run_gym_ctrl.py
+++ baselines/uber_ga/run_gym_ctrl.py
@@ -18,7 +18,7 @@ def train(env_id, num_timesteps, seed):
     max_fitness = -100000
     popsize = 33
     gensize = 100000
-    truncation_size = 20
+    truncation_size = 10
     sigma = 0.1
     eval_iters = 1
     from baselines.uber_ga import mlp_policy, ga_simple
